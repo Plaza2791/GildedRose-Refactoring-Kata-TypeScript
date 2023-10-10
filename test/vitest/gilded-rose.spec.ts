02ad7b25c4c3759f212fcfc68106ae831e8897a9
Andrius <54733285+Recognize8607@users.noreply.github.com>
2023-10-10 14:26:25 +0300

    Add Vitest and Jest tests

--- test/vitest/gilded-rose.spec.ts
+++ test/vitest/gilded-rose.spec.ts
@@ -1,9 +1,36 @@
 import { Item, GildedRose } from '@/gilded-rose';
 
 describe('Gilded Rose', () => {
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
+  const names: string[] = [
+    'foo',
+    'Aged Brie',
+    'Backstage passes to a TAFKAL80ETC concert',
+    'Sulfuras, Hand of Ragnaros'
+  ];
+
+  const sellIns: number[] = [
+    -1,
+    0,
+    1,
+    2,
+    6,
+    11
+  ];
+
+  const qualities: number[] = [
+    0,
+    1,
+    2,
+    49,
+    50
+  ];
+
+  // Cartesian product of all possible inputs
+  const parameters = [names, sellIns, qualities].reduce((a, b) => a.map(x => b.map((y) => x.concat(y))).reduce((a, b) => a.concat(b), []), [[]]);
+
+  it.each(parameters)('should update quality', (name: string, sellIn: number, quality: number) => {
+    const gildedRose = new GildedRose([new Item(name, sellIn, quality)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('fixme');
+    expect(items[0]).toMatchSnapshot();
   });
 });
