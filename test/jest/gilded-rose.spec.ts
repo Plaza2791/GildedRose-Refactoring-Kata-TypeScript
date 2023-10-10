84d9be27c634b5a2b5be0053c5b333078a83181d
Andrius <54733285+Recognize8607@users.noreply.github.com>
2023-10-10 15:36:30 +0300

    Setup ESLint, Prettier, husky, lint-staged

--- test/jest/gilded-rose.spec.ts
+++ test/jest/gilded-rose.spec.ts
@@ -1,36 +1,34 @@
-import { Item, GildedRose } from '@/gilded-rose';
+import { Item, GildedRose } from "@/gilded-rose";
 
-describe('Gilded Rose', () => {
+describe("Gilded Rose", () => {
   const names: string[] = [
-    'foo',
-    'Aged Brie',
-    'Backstage passes to a TAFKAL80ETC concert',
-    'Sulfuras, Hand of Ragnaros'
+    "foo",
+    "Aged Brie",
+    "Backstage passes to a TAFKAL80ETC concert",
+    "Sulfuras, Hand of Ragnaros",
   ];
 
-  const sellIns: number[] = [
-    -1,
-    0,
-    1,
-    2,
-    6,
-    11
-  ];
+  const sellIns: number[] = [-1, 0, 1, 2, 6, 11];
 
-  const qualities: number[] = [
-    0,
-    1,
-    2,
-    49,
-    50
-  ];
+  const qualities: number[] = [0, 1, 2, 49, 50];
 
   // Cartesian product of all possible inputs
-  const parameters = [names, sellIns, qualities].reduce((a, b) => a.map(x => b.map((y) => x.concat(y))).reduce((a, b) => a.concat(b), []), [[]]);
+  const parameters = <[[string, number, number]]>(
+    [names, sellIns, qualities].reduce(
+      (a, b) =>
+        a
+          .map((x) => b.map((y: string | number) => x.concat(y)))
+          .reduce((a, b) => a.concat(b), [] as (string | number)[][]),
+      [[]] as (string | number)[][],
+    )
+  );
 
-  it.each(parameters)('should update quality', (name: string, sellIn: number, quality: number) => {
-    const gildedRose = new GildedRose([new Item(name, sellIn, quality)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0]).toMatchSnapshot();
-  });
+  it.each(parameters)(
+    "should update quality",
+    (name: string, sellIn: number, quality: number) => {
+      const gildedRose = new GildedRose([new Item(name, sellIn, quality)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0]).toMatchSnapshot();
+    },
+  );
 });
